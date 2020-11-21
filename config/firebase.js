e8d8e191ac2ba81fa6c8ac1770dc1affbaeb92da
francisfeng <13903131445@163.com>
2020-11-21 13:55:35 -0500

    Add signup api

---
+++ config/firebase.js
@@ -0,0 +1,1 @@
+module.exports = { key: 'AIzaSyDTXl5uquH3DCJTCsM8Bf9jVsuWAi4F0Dg' };
--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,32 @@ const express = require('express');
 const router = express.Router();
 const data = require('../data');
 const userData = data.users;
+const firebase = require('../config/firebase');
+const axios = require('axios');
 
 router.get('/:id', async (req, res) => {
     try {
         const user = await userData.getUserById(req.params.id);
-        res.json(user);
+        res.status(200).json(user);
     } catch (error) {
         res.status(404).json({ error: error });
+    }
+});
+
+router.post('/signup', async (req, res) => {
+    try {
+        const authData = {
+            email: req.body.email,
+            password: req.body.password,
+            returnSecureToken: true,
+        };
+
+        const url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${firebase.key}`;
+
+        const result = await axios.post(url, authData);
+        res.status(200).json({ data: result.data.idToken });
+    } catch (error) {
+        console.log(error);
     }
 });
 
