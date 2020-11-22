9787e2dc3055b965863adf68435b7e41221ee0ca
francisfeng <13903131445@163.com>
2020-11-22 14:27:41 -0500

    Bug fixed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const configRoutes = require('./routes');
-const cors = require('cores');
+const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
