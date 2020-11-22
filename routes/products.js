dba32d9245571568bc46e27b785440a99b7d39af
francisfeng <13903131445@163.com>
2020-11-22 17:00:19 -0500

    Add auth to products

--- routes/products.js
+++ routes/products.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const data = require('../data');
-// const userData = data.users;
 const productData = data.products;
 const questionData = data.questions;
-// const orderData = data.orders;
-// const firebase = require('../config/firebase');
-// const axios = require('axios');
-// const inputChecker = require('../utility/inputChecker');
+const { authenticated } = require('../utility/authMiddleware');
 
 //seller发布一个新product
-router.post('/', async (req, res) => {
+router.post('/', authenticated, async (req, res) => {
     try {
         let sellerId = req.body.sellerId;
         let productName = req.body.productName;
@@ -18,9 +14,11 @@ router.post('/', async (req, res) => {
         let categoryArr = req.body.categoryArr;
         let imageUrl = req.body.imageUrl;
         let stock = Number(req.body.stock);
-        if (stock <= 0) throw 'stock should > 0';
         let price = Number(req.body.price);
+
+        if (stock <= 0) throw 'stock should > 0';
         if (price <= 0) throw 'price should > 0';
+
         let newProduct = await productData.createPoduct(
             sellerId,
             productName,
@@ -37,7 +35,7 @@ router.post('/', async (req, res) => {
 });
 
 //获取全部商品
-router.get('/', async (req, res) => {
+router.get('/', authenticated, async (req, res) => {
     try {
         let allProduct = await productData.getAllProduct();
         res.status(200).json(allProduct);
@@ -47,7 +45,7 @@ router.get('/', async (req, res) => {
 });
 
 //获取id对应的商品，返回此product
-router.get('/:id', async (req, res) => {
+router.get('/:id', authenticated, async (req, res) => {
     try {
         let productGoal = await productData.getProductById(req.params.id);
         res.status(200).json(productGoal);
@@ -57,11 +55,12 @@ router.get('/:id', async (req, res) => {
 });
 
 //提问，返回这个问题
-router.post('/questions', async (req, res) => {
+router.post('/questions', authenticated, async (req, res) => {
     try {
         let productId = req.body.productId;
         let nickName = req.body.nickName;
         let question = req.body.question;
+
         let questionCreated = await questionData.createQuestion(
             productId,
             nickName,
@@ -74,10 +73,11 @@ router.post('/questions', async (req, res) => {
 });
 
 //回答，返回被回答的问题与答案
-router.post('/answer', async (req, res) => {
+router.post('/answer', authenticated, async (req, res) => {
     try {
         let questionId = req.body.questionId;
         let answer = req.body.answer;
+
         let answerGiven = await questionData.giveAnswer(questionId, answer);
         res.status(200).json(answerGiven);
     } catch (error) {
@@ -86,10 +86,11 @@ router.post('/answer', async (req, res) => {
 });
 
 //修改价格，返回修改后的product
-router.patch('/:id', async (req, res) => {
+router.patch('/:id', authenticated, async (req, res) => {
     try {
         let productId = req.params.id;
         let newPrice = Number(req.body.price);
+
         let updatedProduct = await productData.updatePrice(productId, newPrice);
         res.status(200).json(updatedProduct);
     } catch (error) {
@@ -98,9 +99,10 @@ router.patch('/:id', async (req, res) => {
 });
 
 //删除问题，返回被删除的问题
-router.delete('/quesitons/:id', async (req, res) => {
+router.delete('/quesitons/:id', authenticated, async (req, res) => {
     try {
         let questionId = req.params.id;
+
         let questionDeleted = await questionData.deleteOneQuestion(questionId);
         res.status(200).json(questionDeleted);
     } catch (error) {
@@ -109,9 +111,10 @@ router.delete('/quesitons/:id', async (req, res) => {
 });
 
 //删除商品（此router会同时删除该商品下所有的question），返回被删除的商品与被删除的问题
-router.delete('/:id', async (req, res) => {
+router.delete('/:id', authenticated, async (req, res) => {
     try {
         let productId = req.params.id;
+
         let productDeleted = await productData.deleteProduct(productId);
         let questionsDeleted = await questionData.deleteAllQuestionInProduct(
             productId
@@ -123,9 +126,10 @@ router.delete('/:id', async (req, res) => {
 });
 
 //查询此用户作为seller发布的所有product,返回一个数组
-router.get('/user/:id', async (req, res) => {
+router.get('/user/:id', authenticated, async (req, res) => {
     try {
         let sellerId = req.params.id;
+
         let sellerProducts = await productData.getProductsBySellerId(sellerId);
         res.status(200).json(sellerProducts);
     } catch (error) {
