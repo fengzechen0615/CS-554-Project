355afca6d0d6f3e6fe903145b901e6b5600ff31b
francisfeng <13903131445@163.com>
2020-11-22 00:35:11 -0500

    Bug fix

--- data/index.js
+++ data/index.js
@@ -1,4 +1,7 @@
 const userData = require('./users');
+const productData = require('./products');
+const questionData = require('./questions');
+const orderData = require('./orders');
 
 module.exports = {
     users: userData,
