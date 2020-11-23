47296d3d639d83fddc7c5a02eb7443edff5b2756
francisfeng <13903131445@163.com>
2020-11-22 19:17:26 -0500

    Update products.js

--- data/products.js
+++ data/products.js
@@ -11,21 +11,58 @@ async function createPoduct(
     stock,
     price
 ) {
-    if (!sellerId || typeof sellerId !== 'string')
-        throw 'you should input a string as sellerId';
-    if (!productName || typeof productName !== 'string')
-        throw 'you should input a string as productName';
-    if (!description || typeof description !== 'string')
-        throw 'you should input a string as the content';
-    if (!catagoryArr || !Array.isArray(catagoryArr))
-        throw 'You must provide an array of catagories';
-    if (!imageUrl || typeof imageUrl !== 'string')
-        throw 'you should input a string as imageUrl';
-    if (!stock || typeof stock !== 'number')
-        throw 'You must provide a number as stock';
-    if (!price || typeof price !== 'number')
-        throw 'You must provide a number as price';
+    if (!sellerId || typeof sellerId !== 'string') {
+        throw {
+            status: 400,
+            errorMessage: 'you should input a string as sellerId',
+        };
+    }
+    if (!productName || typeof productName !== 'string') {
+        throw {
+            status: 400,
+            errorMessage: 'you should input a string as productName',
+        };
+    }
+    if (!description || typeof description !== 'string') {
+        throw {
+            status: 400,
+            errorMessage: 'you should input a string as the content',
+        };
+    }
+    if (!catagoryArr || !Array.isArray(catagoryArr)) {
+        throw {
+            status: 400,
+            errorMessage: 'You must provide an array of catagories',
+        };
+    }
+    if (!imageUrl || typeof imageUrl !== 'string') {
+        throw {
+            status: 400,
+            errorMessage: 'you should input a string as imageUrl',
+        };
+    }
+    if (!stock || typeof stock !== 'number') {
+        throw {
+            status: 400,
+            errorMessage: 'You must provide a number as stock',
+        };
+    }
+    if (!price || typeof price !== 'number') {
+        throw {
+            status: 400,
+            errorMessage: 'You must provide a number as price',
+        };
+    }
+
+    if (stock <= 0) {
+        throw { status: 400, errorMessage: 'stock should > 0' };
+    }
+    if (price <= 0) {
+        throw { status: 400, errorMessage: 'price should > 0' };
+    }
+
     let productCollection = await products();
+
     let newProduct = {
         _id: uuid.v4(),
         sellerId: sellerId,
@@ -37,45 +74,70 @@ async function createPoduct(
         price: price,
         date: new Date(),
     };
+
     let insertInfo = await productCollection.insertOne(newProduct);
-    if (insertInfo.insertedCount === 0)
-        throw 'Something wrong when creating the product';
+    if (insertInfo.insertedCount === 0) {
+        throw {
+            status: 400,
+            errorMessage: 'Something wrong when creating the product',
+        };
+    }
     let newId = insertInfo.insertedId;
-    let productCreated = await getProductById(newId);
-    return productCreated;
+    return { status: 200, result: (await getProductById(newId)).result };
 }
 
 async function getAllProduct() {
     let productCollection = await products();
     let allProducts = await productCollection.find({}).toArray();
-    return allProducts;
+    return { status: 200, reuslt: allProducts };
 }
 
 async function getProductById(id) {
-    if (!id || typeof id !== 'string')
-        throw 'You must provide an id to search for Product';
+    if (!id || typeof id !== 'string') {
+        throw {
+            status: 400,
+            errorMessage: 'You must provide an id to search for Product',
+        };
+    }
+
     let productCollection = await products();
-    // let objId = ObjectId.createFromHexString(id);
+
     let productGoal = await productCollection.findOne({ _id: id });
-    if (productGoal === null) throw 'No Product with that id';
-    return productGoal;
+    if (productGoal === null) {
+        throw { status: 404, errorMessage: `No Product with that ${id}` };
+    }
+    return { status: 200, result: productGoal };
 }
 
 async function getProductsBySellerId(sellerId) {
-    if (!sellerId || typeof sellerId !== 'string')
-        throw 'You must provide an sellerId to search for his Products';
+    if (!sellerId || typeof sellerId !== 'string') {
+        throw {
+            error: 400,
+            errorMessage:
+                'You must provide an sellerId to search for his Products',
+        };
+    }
+
     let productCollection = await products();
+
     let productArr = await productCollection
         .find({ sellerId: sellerId })
         .toArray();
-    return productArr;
+    return { status: 200, reuslt: productArr };
 }
 
 async function updatePrice(id, price) {
     if (!id || typeof id !== 'string')
-        throw 'You must provide an id to uodate Product';
-    if (!price || typeof price !== 'number')
-        throw 'You must provide a number as price';
+        throw {
+            status: 400,
+            errorMessage: 'You must provide an id to uodate Product',
+        };
+    if (!price || typeof price !== 'number') {
+        throw {
+            status: 400,
+            errorMessage: 'You must provide a number as price',
+        };
+    }
     let productCollection = await products();
     let updateInfo = await productCollection.updateOne(
         { _id: id },
@@ -86,21 +148,33 @@ async function updatePrice(id, price) {
         }
     );
     if (!updateInfo.matchedCount && !updateInfo.modifiedCount) {
-        throw 'Could not update the price';
+        throw { error: 500, errorMessage: 'Could not update the price' };
     }
-    return await getProductById(id);
+    return { status: 200, result: (await getProductById(id)).result };
 }
 
 async function updateStock(id, dealNumber) {
-    if (!id || typeof id !== 'string')
-        throw 'You must provide an id to uodate Product';
-    if (!dealNumber || typeof dealNumber !== 'number')
-        //dealAmout指交易量
-        throw 'You must provide a number as dealNumber';
-    let currentProduct = getProductById(id);
+    if (!id || typeof id !== 'string') {
+        throw {
+            status: 400,
+            errorMessage: 'You must provide an id to uodate Product',
+        };
+    }
+    if (!dealNumber || typeof dealNumber !== 'number') {
+        throw {
+            status: 400,
+            errorMessage: 'You must provide a number as dealNumber',
+        };
+    }
+
+    let currentProduct = (await getProductById(id)).result;
     let currentStock = currentProduct.stock;
-    if (currentStock - dealNumber < 0) throw 'dealNumber > currentStock!';
+    if (currentStock - dealNumber < 0) {
+        throw { status: 500, errorMessage: 'dealNumber > currentStock!' };
+    }
+
     let productCollection = await products();
+
     let updateInfo = await productCollection.updateOne(
         { _id: id },
         {
@@ -110,20 +184,30 @@ async function updateStock(id, dealNumber) {
         }
     );
     if (!updateInfo.matchedCount && !updateInfo.modifiedCount) {
-        throw 'Could not update the stock';
+        throw { status: 500, errorMessage: 'Could not update the stock' };
     }
-    return await getProductById(id);
+    return { status: 200, result: (await getProductById(id)).reuslt };
 }
 
 async function deleteProduct(id) {
-    if (!id || typeof id !== 'string')
-        throw 'You must provide an id to uodate Product';
+    if (!id || typeof id !== 'string') {
+        throw {
+            status: 400,
+            errorMessage: 'You must provide an id to uodate Product',
+        };
+    }
+
     let productCollection = await products();
-    let productToDelete = await getProductById(id);
+
+    let productToDelete = (await getProductById(id)).result;
     let deletionInfo = await productCollection.removeOne({ _id: id });
-    if (deletionInfo.deletedCount === 0)
-        throw `Could not delete the product with this id`;
-    return productToDelete;
+    if (deletionInfo.deletedCount === 0) {
+        throw {
+            status: 500,
+            errorMessage: `Could not delete the product with this ${id}`,
+        };
+    }
+    return { status: 200, result: productToDelete };
 }
 
 module.exports = {
