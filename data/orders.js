9f89b8c67eced2b3a131330374fee3ce99bb0ed6
francisfeng <13903131445@163.com>
2020-11-22 20:31:10 -0500

    Update orders.js

--- data/orders.js
+++ data/orders.js
@@ -13,23 +13,57 @@ async function createOrder(
     description,
     imgUrl
 ) {
-    if (!productId || typeof productId !== 'string')
-        throw 'you should input a string as productId';
-    if (!sellerId || typeof sellerId !== 'string')
-        throw 'you should input a string as sellerId';
-    if (!buyerId || typeof buyerId !== 'string')
-        throw 'you should input a string as buyerId';
-    if (!adress || typeof adress !== 'string')
-        throw 'you should input a string as adress';
-    if (!price || typeof price !== 'number')
-        throw 'you should input a numer as the price';
-    if (!dealNumber || typeof dealNumber !== 'number')
-        throw 'you should input a numer as the dealNumber';
-    if (!description || typeof description !== 'string')
-        throw 'you should input a string as description';
-    if (!imgUrl || typeof imgUrl !== 'string')
-        throw 'you should input a string as imgUrl';
+    if (!productId || typeof productId !== 'string') {
+        throw {
+            status: 200,
+            errorMessage: 'you should input a string as productId',
+        };
+    }
+    if (!sellerId || typeof sellerId !== 'string') {
+        throw {
+            status: 200,
+            errorMessage: 'you should input a string as sellerId',
+        };
+    }
+    if (!buyerId || typeof buyerId !== 'string') {
+        throw {
+            status: 200,
+            errorMessage: 'you should input a string as buyerId',
+        };
+    }
+    if (!adress || typeof adress !== 'string') {
+        throw {
+            status: 200,
+            errorMessage: 'you should input a string as adress',
+        };
+    }
+    if (!price || typeof price !== 'number') {
+        throw {
+            status: 200,
+            errorMessage: 'you should input a numer as the price',
+        };
+    }
+    if (!dealNumber || typeof dealNumber !== 'number') {
+        throw {
+            status: 200,
+            errorMessage: 'you should input a numer as the dealNumber',
+        };
+    }
+    if (!description || typeof description !== 'string') {
+        throw {
+            status: 200,
+            errorMessage: 'you should input a string as description',
+        };
+    }
+    if (!imgUrl || typeof imgUrl !== 'string') {
+        throw {
+            status: 200,
+            errorMessage: 'you should input a string as imgUrl',
+        };
+    }
+
     let orderCollection = await orders();
+
     let newOrder = {
         _id: uuid.v4(),
         productId: productId,
@@ -44,51 +78,88 @@ async function createOrder(
         description: description,
         imgUrl: imgUrl,
     };
+
     let insertInfo = await orderCollection.insertOne(newOrder);
-    if (insertInfo.insertedCount === 0)
-        throw 'Something wrong when creating the Order';
+    if (insertInfo.insertedCount === 0) {
+        throw {
+            status: 500,
+            errorMessage: 'Something wrong when creating the Order',
+        };
+    }
     let newId = insertInfo.insertedId;
-    let orderCreated = await getOrderById(newId);
-    return orderCreated;
+    return { status: 200, result: (await getOrderById(newId)).result };
 }
 
 async function getAllOrder() {
     let orderCollection = await orders();
     let allOrder = await orderCollection.find({}).toArray();
-    return allOrder;
+    return { status: 200, result: allOrder };
 }
 
 async function getOrderById(id) {
-    if (!id || typeof id !== 'string')
-        throw 'You must provide an order id to search for an order';
+    if (!id || typeof id !== 'string') {
+        throw {
+            status: 400,
+            errorMessage: 'You must provide an order id to search for an order',
+        };
+    }
+
     let orderCollection = await orders();
+
     let orderGoal = await orderCollection.findOne({ _id: id });
-    if (orderGoal === null) throw 'No order with that id';
-    return orderGoal;
+    if (orderGoal === null) {
+        throw { status: 404, errorMessage: `No order with that ${id}` };
+    }
+    return { status: 200, result: orderGoal };
 }
 
 async function getOrderBySellerId(sellerId) {
-    if (!sellerId || typeof sellerId !== 'string')
-        throw 'You must provide an seller id to search his selling history';
+    if (!sellerId || typeof sellerId !== 'string') {
+        throw {
+            status: 400,
+            errorMessage:
+                'You must provide an seller id to search his selling history',
+        };
+    }
+
     let orderCollection = await orders();
+
     let orderArr = await orderCollection.find({ sellerId: sellerId }).toArray();
-    return orderArr;
+
+    return { status: 200, result: orderArr };
 }
 
 async function getOrderByBuyerId(buyerId) {
-    if (!buyerId || typeof buyerId !== 'string')
-        throw 'You must provide an buyerId to search his buyer history';
+    if (!buyerId || typeof buyerId !== 'string') {
+        throw {
+            status: 400,
+            errorMessage:
+                'You must provide an buyerId to search his buyer history',
+        };
+    }
+
     let orderCollection = await orders();
+
     let orderArr = await orderCollection.find({ buyerId: buyerId }).toArray();
-    return orderArr;
+    return { status: 200, result: orderArr };
 }
 
 async function setOrderIsCompeleted(id, isCompleted) {
-    if (!id || typeof id !== 'string')
-        throw 'You must provide an order id to search for an order';
-    if (!isCompleted || typeof isCompleted !== 'boolean')
-        throw 'You must set isCompleted true or false';
+    if (!id || typeof id !== 'string') {
+        throw {
+            status: 400,
+            errorMessage: 'You must provide an order id to search for an order',
+        };
+    }
+    if (!isCompleted || typeof isCompleted !== 'boolean') {
+        throw {
+            status: 400,
+            errorMessage: 'You must set isCompleted true or false',
+        };
+    }
+
     let orderCollection = await orders();
+
     let updateInfo = await orderCollection.updateOne(
         { _id: id },
         {
@@ -97,10 +168,11 @@ async function setOrderIsCompeleted(id, isCompleted) {
             },
         }
     );
+
     if (!updateInfo.matchedCount && !updateInfo.modifiedCount) {
-        throw 'Could not set isCompleted';
+        throw { status: 500, errorMessage: 'Could not set isCompleted' };
     }
-    return await getOrderById(id);
+    return { status: 200, result: (await getOrderById(id)).result };
 }
 
 module.exports = {
