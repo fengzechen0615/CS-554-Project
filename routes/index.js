682b91fb88478d28f7ab5abfcc576430e1cf45a4
Jugram <dchen21@stevens.edu>
2020-11-22 19:49:37 -0500

    update routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const userRoutes = require('./users');
 const productRoutes = require('./products');
+const orderRoutes = require('./orders');
 
 const constructorMethod = (app) => {
     app.use('/v1/users', userRoutes);
     app.use('/v1/products', productRoutes);
+    app.use('/v1/orders', orderRoutes);
 
     app.use('*', (req, res) => {
         res.status(404).json({ error: 'page not found' });
