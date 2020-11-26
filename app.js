0df9d5cd6078d9661f7f0b45a9ec091f36a54e63
htkz <htkzmo@gmail.com>
2020-11-25 20:48:57 -0500

    Set credential for cors

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const session = require('express-session');
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+        origin: 'http://localhost:3001',
+        credentials: true,
+    })
+);
 app.use(
     session({
         name: 'AuthCookie',
