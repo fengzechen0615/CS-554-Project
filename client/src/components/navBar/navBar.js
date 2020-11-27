1a303d5b901df8ad48c25f17a5f023e91f75722e
htkz <htkzmo@gmail.com>
2020-11-26 22:31:58 -0500

    Active link for current page

--- client/src/components/navBar/navBar.js
+++ client/src/components/navBar/navBar.js
@@ -1,24 +1,43 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
+import { useLocation } from 'react-router-dom';
 
-export default function NavBar() {
+export default function NavBar(props) {
+    const location = useLocation();
+
     return (
         <Navbar bg='dark' variant='dark'>
             <Navbar.Brand as={Link} to='/products'>
                 Navbar
             </Navbar.Brand>
             <Nav className='mr-auto'>
-                <Nav.Link as={Link} to='/products'>
+                <Nav.Link
+                    as={Link}
+                    to='/products'
+                    active={location.pathname === '/products'}
+                >
                     Products
                 </Nav.Link>
-                <Nav.Link as={Link} to='/user/info'>
+                <Nav.Link
+                    as={Link}
+                    to='/user/info'
+                    active={location.pathname === '/user/info'}
+                >
                     Info
                 </Nav.Link>
-                <Nav.Link as={Link} to='/user/selling'>
+                <Nav.Link
+                    as={Link}
+                    to='/user/selling'
+                    active={location.pathname === '/user/selling'}
+                >
                     Selling
                 </Nav.Link>
-                <Nav.Link as={Link} to='/user/purchase'>
+                <Nav.Link
+                    as={Link}
+                    to='/user/purchase'
+                    active={location.pathname === '/user/puchase'}
+                >
                     Pruchase
                 </Nav.Link>
                 <Nav.Link as={Link} to='/logout'>
