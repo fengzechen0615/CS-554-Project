eda49ba1957e7ff07ce5d052df55a38b87b185b8
htkz <htkzmo@gmail.com>
2020-12-02 21:21:06 -0500

    Add basic info for detail page

--- client/src/app/product/product.js
+++ client/src/app/product/product.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+import { Chip } from '@material-ui/core';
 import { useParams } from 'react-router-dom';
 import { showError } from 'components/sweetAlert/sweetAlert';
 import { getProduct } from 'api/products';
+import './product.css';
 
 export default function Main() {
     const [product, setProduct] = useState({});
@@ -22,5 +24,31 @@ export default function Main() {
         initProduct();
     }, []);
 
-    return <Container className='p-3'>product</Container>;
+    return (
+        <Container className='p-3 mt-5'>
+            <Row>
+                <Col xs={12} md={6}>
+                    <img src={`${product.imageUrl}`} />
+                </Col>
+                <Col xs={12} md={6}>
+                    <h1>{product.productName}</h1>
+                    <p>{product.description}</p>
+                    <p className='m-1'>Price: {product.price}</p>
+                    <p className='m-1'>Stock: {product.stock}</p>
+                    <p className='m-1'>Seller: </p>
+                    <p className='m-1'>
+                        Post Date: {product.date && product.date.split('T')[0]}{' '}
+                    </p>
+                    <div className='m-1'>
+                        Category:{' '}
+                        {product.categoryArr &&
+                            product.categoryArr.length > 0 &&
+                            product.categoryArr.map((cat, idx) => (
+                                <Chip key={idx} label={cat} />
+                            ))}
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
