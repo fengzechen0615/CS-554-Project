1da1734a89a0947cf8d6e54b19d220de08363759
htkz <htkzmo@gmail.com>
2020-12-15 06:22:36 -0500

    Remove test account

--- client/src/auth/login/login.js
+++ client/src/auth/login/login.js
@@ -7,8 +7,8 @@ import { useDispatch } from 'react-redux';
 import { showError } from 'components/sweetAlert/sweetAlert';
 
 export default function Login() {
-    const [email, setEmail] = useState('htkzmo@gmail.com');
-    const [password, setPassword] = useState('12345678');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const [showAlert, setShowAlert] = useState(false);
     const dispatch = useDispatch();
 
